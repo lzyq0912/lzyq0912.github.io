3efbb2804d5c8b639a34217016078296070d8593
lzyq0912 <1197795731@qq.com>
2022-09-17 19:52:42 +0800

    Site updated: 2022-09-17 19:52:40

--- js/duration.js
+++ js/duration.js
@@ -1,6 +1,6 @@
 var now = new Date();
 function createtime() {
-    var grt= new Date("09/16/2022 23:00:00");//�ڴ˴��޸���Ľ�վʱ�䣬��ʽ����/��/�� ʱ:��:��
+    var grt= new Date("09/16/2022 23:00:00");//在此处修改你的建站时间，格式：月/日/年 时:分:秒
     now.setTime(now.getTime()+250);
     days = (now - grt ) / 1000 / 60 / 60 / 24; dnum = Math.floor(days);
     hours = (now - grt ) / 1000 / 60 / 60 - (24 * dnum); hnum = Math.floor(hours);
@@ -8,7 +8,7 @@ function createtime() {
     mnum = Math.floor(minutes); if(String(mnum).length ==1 ){mnum = "0" + mnum;}
     seconds = (now - grt ) / 1000 - (24 * 60 * 60 * dnum) - (60 * 60 * hnum) - (60 * mnum);
     snum = Math.round(seconds); if(String(snum).length ==1 ){snum = "0" + snum;}
-    document.getElementById("timeDate").innerHTML = "��վ�Ѱ�ȫ���� "+dnum+" �� ";
-    document.getElementById("times").innerHTML = hnum + " Сʱ " + mnum + " �� " + snum + " ��";
+    document.getElementById("timeDate").innerHTML = "本站已安全运行 "+dnum+" 天 ";
+    document.getElementById("times").innerHTML = hnum + " 小时 " + mnum + " 分 " + snum + " 秒";
 }
 setInterval("createtime()",250);
